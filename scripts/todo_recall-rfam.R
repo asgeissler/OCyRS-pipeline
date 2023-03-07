5756a0194349973324fb5926ba0fa259c34ee03c
Adrian Geissler <20241375+asgeissler@users.noreply.github.com>
2023-03-07 11:41:18 +0100

    inc recall statistics

--- scripts/todo_recall-rfam.R
+++ scripts/todo_recall-rfam.R
@@ -1,19 +1,16 @@
 # Purpose:
-# Check for overlaps between motif homologs and cmsearch hits of
+# Check for overlaps between motif alignment sequence positions and cmsearch
+# hits of
 # (A) bacterial Rfam
 # (B) RNIE terminator predictions
-# Also, relate recall rates to motif score metriccs
 
 library(tidyverse)
 library(tidygraph)
 
 in.rfam <- 'data/G_rfam-cmsearch.tsv.gz'
 in.term <- 'data/G2_terminators.tsv.gz'
-in.homologs <- 'data/J_motif-homologs.tsv'
+in.motifs <- 'data/J_motif-aln-seq-pos.tsv'
 in.cats = 'data/J_FDR-categories.tsv'
-
-in.fdr <- 'data/I_fdr.tsv'
-in.scores <- 'data/H_scores.tsv'
 
 # Colorblind-friendly palettes of the Color Universal Design
 # https://riptutorial.com/r/example/28354/colorblind-friendly-palettes
@@ -25,9 +22,8 @@ cbbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442",
 
 rfam <- read_tsv(in.rfam)
 term <- read_tsv(in.term)
-homologs <- read_tsv(in.homologs)
-fdr <- read_tsv(in.fdr)
-scores <- read_tsv(in.scores)
+motifs <- read_tsv(in.motifs)
+cats <- read_tsv(in.cats)
 
 
 ################################################################################
@@ -39,9 +35,8 @@ list(
   Rfam = rfam %>%
     select(- tax_bio, - gc) %>%
     rename(name = rf, rfam = name),
-  'CMfinder motif' = homologs %>%
-    rename(chr = tax.bio.chr, name = motif) %>%
-    select(- c(tax_bio, gc, cms.row))
+  'Candidate motif' = motifs %>%
+    rename(name = motif)
 ) %>%
   map2(names(.), ~ mutate(.x, type = .y)) %>%
   bind_rows() %>%
@@ -79,95 +74,16 @@ ranges %>%
   as_tibble() -> overlaps
 
 ################################################################################
-# First, filter out overlaps between rfam hits etc
+# Determine bp cutoff for recall
 
 overlaps %>%
-  filter(row.x < row.y, type.x == type.y) -> over.type
-
-cutoff.redundant <- 20
-
-over.type %>%
-  # ggplot(aes(jaccard, color = orientation)) +
-  ggplot(aes(overlap, color = orientation)) +
-  annotation_logticks(
-    sides = 'b',
-    short = unit(0.5, "cm"),
-    mid = unit(0.8, "cm"),
-    long = unit(1, "cm"),
-  ) +
-  scale_x_log10(minor_breaks = FALSE) +
-  stat_ecdf(size = 1.2) +
-  ggsci::scale_color_jama() +
-  geom_vline(xintercept = cutoff.redundant, color = 'blue') +
-  # geom_vline(xintercept = 40, color = 'red') +
-  # scale_x_continuous(breaks = seq(0, 1, .1)) +
-  scale_y_continuous(breaks = seq(0, 1, .1)) +
-  xlab('Overlap between cmsearch hits [bps]') +
-  ylab('Emp. cum. density') +
-  facet_wrap( ~ type.x) +
-  theme_bw(18) +
-  theme(legend.position = 'bottom')
-
-ggsave('foo1.jpeg', width = 12, height = 6, dpi = 500)
-
-# Determine overlaps above cutoff
-over.type %>%
-  filter(overlap >= cutoff.redundant) %>%
-  select(from = row.x, to = row.y) %>%
-  mutate_all(as.character) -> es
-  
-# Group of overlapping form graph
-tbl_graph(edges = es, directed = FALSE) %>%
-  activate(nodes) %>%
-  mutate(grp = group_components()) %>%
-  as_tibble %>%
-  mutate(row = as.integer(name)) %>%
-  left_join(as_tibble(ranges), 'row') -> overlapping.hits
-
-# Min e-value of overlapping group
-overlapping.hits %>%
-  group_by(grp) %>%
-  summarize(min.e = min(evalue)) %>%
-  ungroup %>%
-  # In order to resolve ties, also choose max score
-  left_join(overlapping.hits, 'grp') %>%
-  filter(min.e == evalue) %>%
-  group_by(grp, min.e) %>%
-  summarize(max.score = max(score)) %>%
-  ungroup %>%
-  # Those with larger evalue than min per group are to be excluded
-  left_join(overlapping.hits, 'grp') %>%
-  filter( (min.e < evalue) | (score < max.score) ) %>%
-  select(row) %>%
-  unique -> to.exclude
-
-ranges %>%
-  filter(! (row %in% to.exclude$row)) -> ranges.nonredundant
-
-overlaps %>%
-  anti_join(to.exclude, c('row.x' = 'row')) %>%
-  anti_join(to.exclude, c('row.y' = 'row')) -> overlaps.nonredundant
-
-# length(ranges.nonredundant) / length(ranges)
-# nrow(overlaps.nonredundant) / nrow(overlaps)
-
-# Export non-redundant set
-ranges.nonredundant %>%
-  as_tibble() %>%
-  rename(tax.bio.chr = seqnames) %>%
-  select(- row, - len) %>%
-  write_tsv('foo.tsv.gz')
-  
-################################################################################
-# Overlap in non-redundant set ahead of recall
-
-overlaps.nonredundant %>%
   filter(
-    type.x == 'CMfinder motif',
+    type.x == 'Candidate motif',
     type.x != type.y
   ) -> over.between
 
-cutoff.recall.overlap <- 5
+cutoff.recall.overlap.high <- 10
+cutoff.recall.overlap.low <- 1
 
 over.between %>%
   ggplot(aes(overlap, color = orientation)) +
@@ -177,14 +93,16 @@ over.between %>%
   scale_x_log10(breaks = c(1, 5, 10, 50, 100, 150)) +
   annotation_logticks(sides = 'b') +
   scale_y_continuous(breaks = seq(0, 1, .1)) +
-  geom_vline(xintercept = cutoff.recall.overlap, color = 'blue') +
-  xlab('Overlap with CMfinder motifs [bps]') +
-  ylab('Emp. cum. density') +
+  geom_vline(xintercept = cutoff.recall.overlap.low, color = 'red') +
+  geom_vline(xintercept = cutoff.recall.overlap.high, color = 'blue') +
+  xlab('Overlap with candidate motif [bp]') +
+  ylab('Empirical cumulative density') +
   facet_wrap( ~ type.y) +
   theme_bw(18) +
   theme(legend.position = 'bottom')
 
-ggsave('foo2.jpeg', width = 12, height = 6, dpi = 500)
+ggsave('foo2.jpeg',
+       width = 12, height = 6, dpi = 500)
 
 ################################################################################
 # Ahead of recall, query Rfam family types for more informative plots
@@ -213,13 +131,13 @@ rfam.types %>%
 
 ################################################################################
 
-ranges.nonredundant %>%
+ranges %>%
   as_tibble() %>%
   count(name, type, name = 'no.seqs') -> no.seq
 
 
 over.between %>%
-  filter(overlap >= cutoff.recall.overlap) %>%
+  filter(overlap >= cutoff.recall.overlap.high) %>%
   left_join(rfam.types, c('name.y' = 'Family')) %>%
   # count(rfam.y) %>%
   # arrange(desc(n))
@@ -232,14 +150,15 @@ over.between %>%
   select(motif = name.x, type = type.y, type2, name = name.y) %>%
   count(motif, type, type2, name, name = 'no.recall') %>%
   left_join(
-    select(no.seq, motif = name, motif.homologs = no.seqs),
+    select(no.seq, motif = name, motif.seqs = no.seqs),
     'motif'
   ) %>%
   left_join(no.seq, c('name', 'type')) %>%
   mutate(
     recall = no.recall / no.seqs,
-    precision = no.recall / motif.homologs,
-    F1 = 2 * no.recall / ( 2 * no.recall + (no.seqs - no.recall) + (motif.homologs - no.recall))
+    precision = no.recall / motif.seqs,
+    # F1 = 2 TP / (2* TP + FP + FN)
+    F1 = 2 * no.recall / ( 2 * no.recall + (no.seqs - no.recall) + (motif.seqs - no.recall))
   ) -> over.stat
 over.stat %>%
   ggplot(aes(recall, precision, color = type2)) +
@@ -257,6 +176,15 @@ over.stat %>%
 p <- ggExtra::ggMarginal(p, type = 'histogram')
 ggsave('foo3.jpeg', plot = p, width = 10, height = 10, dpi = 500)
 
+over.stat %>%
+  group_by(motif) %>%
+  slice_max(recall) %>%
+  left_join(cats, 'motif') %>%
+  ggplot(aes(recall, precision, color = category)) +
+  geom_point() + facet_wrap(~ type2, scales = 'free')
+  ggplot(aes(type2, precision, color = category)) +
+  geom_boxplot()
+categroy
 
 ###
 
