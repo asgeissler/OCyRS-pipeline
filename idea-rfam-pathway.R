d95ee70fa80e84f4ee19c1dc11f34b84f9b7a316
Adrian Geissler <20241375+asgeissler@users.noreply.github.com>
2023-06-13 11:39:20 +0200

    stefan's extra request

--- idea-rfam-pathway.R
+++ idea-rfam-pathway.R
@@ -89,5 +89,50 @@ dat %>%
 ggsave('baz.jpeg')
 
 ################################################################################
+################################################################################
+# OK. In addition I would like to see the number of search regions with
+# (a) at least one cmsearch hit of the family, and
+# (b) at least one cmsearch hit of the family and the specific KEGG pathway (count one for each KO search region).
+# The log-odds ratio would be: log( b/a ).
 
+rf %>%
+  filter(type == 'Rfam') %>%
+  mutate(term = str_remove(region, '_.*$')) %>%
+  select(name, rfam, region, term) %>%
+  unique %>%
+  left_join(kegg.path.ko, 'term') %>%
+  left_join(kegg.paths, 'path') %>%
+  filter(str_starts(path, 'map')) -> rf.extra
 
+list(
+  
+  rf.extra %>%
+    select(rfam, name, region) %>%
+    unique %>%
+    count(rfam, name, name = 'A. Regions (KEGG orthologs but up/downstream specific) with structure'),
+  
+  rf.extra %>%
+    select(pathway, rfam, name, region) %>%
+    unique %>%
+    count(pathway, rfam, name, name = 'B. Regions (KEGG orthologs but up/downstream specific) in pathway with structure')
+  
+) %>%
+  reduce(left_join) %>%
+  filter(
+    `A. Regions (KEGG orthologs but up/downstream specific) with structure` >= 1,
+    `B. Regions (KEGG orthologs but up/downstream specific) in pathway with structure` >= 1
+  ) %>%
+  mutate(
+    log2.ratio.BA = log2(
+      `B. Regions (KEGG orthologs but up/downstream specific) in pathway with structure` /
+      `A. Regions (KEGG orthologs but up/downstream specific) with structure` 
+    )
+  ) %>%
+  arrange(desc(log2.ratio.BA)) -> foo
+foo %>%
+  write_tsv('stefan-BA-ratio.tsv')
+
+foo %>%
+  ggplot(aes(log2.ratio.BA)) +
+  stat_ecdf() +
+  scale_y_continuous(breaks = seq(0, 1, .1))
