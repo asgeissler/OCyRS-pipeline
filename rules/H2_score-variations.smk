20d43c9f28374f0bec2683e8beb281173c8e36ca
Adrian Geissler <20241375+asgeissler@users.noreply.github.com>
2024-08-06 14:53:40 +0200

    toward additional RNAphylo considerations

---
+++ rules/H2_score-variations.smk
@@ -0,0 +1,72 @@
+# Test the various additional RNAphylo scoring options
+# - For each reference tree
+# - RNA structure alignment implied score
+
+# Score the motifs with
+# data/F_cmfinder/D_search-seqs/{region}/{region}.fna.motif.h1_1
+# data/F_cmfinder/E_search-shuffled/{region}/{region}.fna.motif.h1_1
+# with F_aggregate(wildcards, x)
+
+
+rule H2_pscore:
+    input:
+        # here: {region} is split into {ortho}_{up/downstream side} to get the tree
+        'data/H_symlink_{dir}/{ortho}_{side}.fna.motif.{number}.sto'
+    output:
+        'data/H2_scores/{dir}/{ortho}_{side}.fna.motif.{number}.txt'
+    container:
+        'cmfinder/cmfinder-0.4.1.18.sif'
+    shell:
+        """
+        tmp=$(mktemp -d)
+        echo "STO file:"  > {output}
+        echo "{input}"   >> {output}
+        
+        # run for each reference tree
+        for ref in reference-trees/*tree ; do
+            # tree labels are: "txid.bioproject.name"
+            # for RNAphylo, the '.name' has to be removed
+            i=$(basename $ref .tree)
+            sed -e 's,\.[^.:(),]*:,:,g' $ref > $tmp/$i.txt
+            
+            # compute RNAphylo per tree
+            RNAPhylo --fragmentary --partition     \
+                --partition-close-to-fuzzy-limit 3 \
+                --suspicious-degen-nucs 2          \
+                --ignore-all-gap                   \
+                -t $tmp/$i.txt                     \
+                {input}                            > $tmp/$i.pscore.txt
+                
+            # Store result in output
+            echo "$i"                                    >> {output}
+            grep 'Total .* posterior' $tmp/$i.pscore.txt >> {output}
+        done
+
+        # run once with tree implied by alignment
+        perl $CMfinder/bin/ScoreMotif.pl {input} > $tmp/implied.txt
+        
+        echo "Implied tree"                       >> {output}
+        grep 'RNAphylo score' $tmp/implied.txt    >> {output}
+
+        rm -rf $tmp
+        """
+
+
+
+rule H2_all:
+    input:
+        lambda wild: F_aggregate(wild, 'data/H2_scores/{dir}/{filename}.txt'),
+    output:
+        touch('data/H2_scores/done.flag')
+
+
+rule H2_combine:
+    input:
+        'data/H_scores/done.flag'
+    output:
+        'data/H_scores.tsv'
+    container: 'renv/renv.sif'
+    conda: 'renv'
+    threads: 8
+    script:
+        '../scripts/H_collect-scores.R'
---
+++ scripts/H2_collect.R
@@ -0,0 +1,97 @@
+library(tidyverse)
+
+library(furrr)
+
+plan(multisession)
+################################################################################
+
+xs <-
+  'data/H2_scores/*/*.txt' |>
+  Sys.glob() 
+
+################################################################################
+
+extra.scores <-
+  xs |>
+  future_map(function(i) {
+    # i <- 'data/H2_scores/D_search-seqs/K21929_upstream.fna.motif.h1_4.h2_3.h2_4.txt'
+    i |>
+      read_lines() |>
+      discard(str_detect, 'Total pair posterior') |>
+      str_remove('^RNAphylo score: ') |>
+      str_remove('^Total RNA posterior ') |>
+      str_remove('^data/H_symlink_') |>
+      matrix(ncol = 2, byrow = TRUE) -> res
+    set_names(res[, 2], res[, 1])
+  }) |>
+  bind_rows() |>
+  separate(`STO file:`, c('dir', 'motif'), sep = '/') |>
+  mutate_at(
+    c('Chen_genomic', 'Chen_multigene', 'Cornet', 'Moore', 'Implied tree'),
+    as.numeric
+  )
+
+extra.scores |>
+  write_tsv('data/H2_extra-scores.tsv')
+
+################################################################################
+
+local.phylo <-
+  'data/H_scores.tsv' |>
+  read_tsv()
+
+################################################################################
+
+extra.scores |>
+  pivot_longer(- c(dir, motif))  |>
+  select(dir, motif, metric = name, rnaphylo = value) |>
+  bind_rows(
+    local.phylo |>
+      transmute(dir, motif, metric = 'Local phylogeny', rnaphylo = RNAphylo) |>
+      filter(dir != 'G_rfam-bacteria-seeds')
+  ) |>
+  filter(rnaphylo > 0) |>
+  mutate(
+    x = ifelse(dir == 'D_search-seqs', 'Biological motif', 'Random background'),
+    metric2 = paste(x, metric, sep = ';') |>
+      fct_reorder(rnaphylo),
+    metric = paste(metric, sep = ';') |>
+      fct_reorder(rnaphylo)
+  ) -> foo
+
+p1 <-
+  foo |>
+  ggplot(aes(rnaphylo, color = metric)) +
+  stat_ecdf(size = 1.2) +
+  scale_x_log10() +
+  ggsci::scale_color_jama(name = 'Scoring relative to') +
+  xlab('RNAphylo score\n(non-positive values omitted)') +
+  ylab('Empirical cumulative density') +
+  facet_wrap(~ x) +
+  theme_bw(18)
+
+p2 <-
+  foo |>
+  ggplot(aes(metric2, rnaphylo, color = metric, fill = metric)) +
+  geom_violin() +
+  geom_boxplot(fill = 'white', color = 'black', width = .1) +
+  scale_y_log10() +
+  ggsci::scale_color_jama(name = NULL) +
+  ggsci::scale_fill_jama(name = NULL) +
+  xlab('Scoring relative to\n(arranged by median)') +
+  ylab('RNAphylo score\n(non-positive values omitted)') +
+  scale_x_discrete(labels = foo |>
+                     select(metric, metric2) |>
+                     mutate_all(as.character) |>
+                     unique() |>
+                     with(set_names(metric, metric2))) +
+  facet_wrap(~ x, scales = 'free_x') +
+  theme_bw(18) +
+  theme(legend.position = 'hide')
+
+
+cowplot::plot_grid(p1, p2, ncol = 1)
+ggsave('foo.jpeg', width = 22, height = 10)
+
+################################################################################
+################################################################################
